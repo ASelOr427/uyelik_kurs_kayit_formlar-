9f9e93a40eb344e610d01d011b3f2db840cf7aca
ASelOr427 <a.sel.or.427@hotmail.com>
2025-02-03 15:44:49 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,9 @@
 
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+  import { getAuth } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js';
+  import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js';
+  import { getDatabase} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
