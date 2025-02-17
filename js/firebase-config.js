29496f226e2ba4b8f9acea3f2d5560967286d95f
ASelOr427 <a.sel.or.427@hotmail.com>
2025-02-17 18:33:59 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,6 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-  import { onAuthStateChanged, getAuth, createUserWithEmailAndPassword as firebaseCreateUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js';
+  import { signOut, onAuthStateChanged, getAuth, createUserWithEmailAndPassword as firebaseCreateUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js';
   import { getFirestore, doc, setDoc, getDoc, collection, query, where, getDocs } from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js';
   import { getDatabase} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
 
@@ -28,5 +28,6 @@
   firebaseCreateUserWithEmailAndPassword as createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   doc, setDoc, getDoc, collection, query, where, getDocs,
-  onAuthStateChanged
+  onAuthStateChanged,
+  signOut  
 };
